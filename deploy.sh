4d56ef06be2dbc4b9ad4fd9ce90f4b20d3ef4d67
Vladislav Gordeev <gordeevladislav@mail.ru>
2020-08-07 13:58:08 +0300

    add link to deployed project

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+# если вы публикуете на пользовательский домен
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:panicswtch/vue-to-do.git master:gh-pages
+
+cd -
